73ebf7a9e35564b0aac43311c88b79f7c68f7bed
zjafarian <zjafarian88@gmail.com>
2020-08-17 17:58:18 +0430

    Initial commit

---
+++ app/src/main/java/com/example/twogameinone/controller/Games.java
@@ -0,0 +1,16 @@
+package com.example.twogameinone.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.twogameinone.R;
+
+public class Games extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games);
+    }
+}
